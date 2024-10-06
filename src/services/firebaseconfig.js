dde3dbdd4700df85233d90a61cdbe4cbc107f5e1
EmreKaya0860 <h.emrekaya2207@gmail.com>
2024-10-06 14:29:30 +0300

    added firebase configuration

---
+++ src/services/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7-tyaHSYeqQVTqeUyJhmMIeQWKChbXjs",
+  authDomain: "chatwithme-d9f86.firebaseapp.com",
+  projectId: "chatwithme-d9f86",
+  storageBucket: "chatwithme-d9f86.appspot.com",
+  messagingSenderId: "407136523929",
+  appId: "1:407136523929:web:44577cf48e190b51444a08",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
